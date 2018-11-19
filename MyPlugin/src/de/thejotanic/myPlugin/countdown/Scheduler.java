d56818aa2cbce2c8307980b83915ce301d9ef09a
Paul Wedeck <paulwedeck@gmail.com>
2018-11-19 19:45:53 +0100

    add TextDrawer and rewrite Scheduler note: numbers from 3-9 are still needed to properly work

--- MyPlugin/src/de/thejotanic/myPlugin/countdown/Scheduler.java
+++ MyPlugin/src/de/thejotanic/myPlugin/countdown/Scheduler.java
@@ -1,230 +1,81 @@
 package de.thejotanic.myPlugin.countdown;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 
-import de.thejotanic.myPlugin.locations.Numbers;
+import de.thejotanic.myPlugin.TextDrawer;
 import de.thejotanic.myPlugin.main.Main;
 
-public class Scheduler extends Numbers implements CommandExecutor {
-	
-	private int taskID0;
-	private int taskID1;
-	private int taskID2;
-	private int taskID3;
+public class Scheduler implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
-		if(sender instanceof Player) {
-			Player p = (Player) sender;
-			if(p.hasPermission("myPlugins.countdown")) {
-				if(args.length == 3) {
-					if(Double.valueOf(args[0]) != null && Double.valueOf(args[1]) != null && Double.valueOf(args[2]) != null) {
-						
-						Location loc0 = new Location(p.getWorld(), Double.valueOf(args[0]), Double.valueOf(args[1]), Double.valueOf(args[2]));
-						
-						Location loc1 = new Location(p.getWorld(), loc0.getX(), loc0.getY(), loc0.getZ() - 04);
-						Location loc2 = new Location(p.getWorld(), loc0.getX(), loc0.getY(), loc0.getZ() - 07);
-						Location loc3 = new Location(p.getWorld(), loc0.getX(), loc0.getY(), loc0.getZ() - 10);
-						Location loc4 = new Location(p.getWorld(), loc0.getX(), loc0.getY(), loc0.getZ() - 14);
-						
-						taskID0 = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(), new Runnable() {
-							
-							int minutes = 6;
-							int i = 0;
-							
-							@Override
-							public void run() {
-								
-								minutes--;
-								
-								if(minutes < 5 && minutes >= 0) {
-									onZero(loc0);
-									
-									switch (minutes) {
-									case 4:
-										onFour(loc1);
-										break;
-									case 3:
-										onThree(loc1);
-										break;
-									case 2:
-										onTwo(loc1);
-										break;
-									case 1:
-										onOne(loc1);
-										break;
-									case 0:
-										onZero(loc1);
-										break;
 
-									default:
-										break;
-									}
-									
-									taskID1 = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(), new Runnable() {
-										
-										int tenseconds = 6;
-												
-										@Override
-										public void run() {
-											
-											tenseconds--;
-											
-											switch (tenseconds) {
-											case 5:
-												onFive(loc3);
-												break;
-											case 4:
-												onFour(loc3);
-												break;
-											case 3:
-												onThree(loc3);
-												break;
-											case 2:
-												onTwo(loc3);
-												break;
-											case 1:
-												onOne(loc3);
-												break;
-											case 0:
-												onZero(loc3);
-												break;
-
-											default:
-												break;
-											}
-											
-											taskID2 = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(), new Runnable() {
-													
-												int seconds = 10;
-													
-												@Override
-												public void run() {
-													
-													seconds--;
-													
-													switch (seconds) {
-													case 9:
-														onNine(loc4);
-														break;
-													case 8:
-														onEight(loc4);
-														break;
-													case 7:
-														onSeven(loc4);
-														break;
-													case 6:
-														onSix(loc4);
-														break;
-													case 5:
-														onFive(loc4);
-														break;
-													case 4:
-														onFour(loc4);
-														break;
-													case 3:
-														onThree(loc4);
-														break;
-													case 2:
-														onTwo(loc4);
-														break;
-													case 1:
-														onOne(loc4);
-														break;
-													case 0:
-														onZero(loc4);
-														break;
-
-													default:
-														break;
-													}
-													
-													taskID3 = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(), new Runnable() {
-														
-														int halfseconds = 2;
-														
-														@Override
-														public void run() {
-															
-															halfseconds--;
-															
-															switch(halfseconds) {
-															case 1:
-																onDp(loc2);
-																break;
-															case 0:
-																onDelete(loc2);
-																break;
-															default:
-																break;
-															}
-															
-															if(halfseconds <= 0) {
-																Bukkit.getScheduler().cancelTask(taskID3);
-																if(seconds <= 0) {
-																	Bukkit.getScheduler().cancelTask(taskID2);
-																	if(tenseconds <= 0) {
-																		Bukkit.getScheduler().cancelTask(taskID1);
-																		if(minutes <= 0) {
-																			minutes--;
-																			onDp(loc2);
-																		}
-																		
-																	}
-																	
-																}
-																
-															}
-															
-														}
-														
-													}, 0, 10);
-													
-												}
-												
-											}, 0, 20);
-											
-										}
-											
-									}, 0, 200);	
-									
-								} else if(minutes == 5) {
-									Bukkit.broadcastMessage("§aTimer auf §6" + minutes + "§a Minuten");
-									onZero(loc0);
-									onFive(loc1);
-									onDp(loc2);
-									onZero(loc3);
-									onZero(loc4);
-								}
-								
-								else if(minutes < 0 && i == 1) {
-									Bukkit.getScheduler().cancelTask(taskID0);
-									onDelete(loc0);
-									onDelete(loc1);
-									onDelete(loc2);
-									onDelete(loc3);
-									onDelete(loc4);
-								}
-								else
-									i++;
-							}
-							
-						}, 0, 1200);
-						
-					} else
-						p.sendMessage("§cBitte benutze Zahlen für /countdown <x> <y> <z>");	
-				} else
-					p.sendMessage("§cBitte benutze /countdown <x> <y> <z>");
-			} else
-				p.sendMessage("§cDazu hast du keine Rechte!");
+		// player only
+		if(!(sender instanceof Player)) {
+			return false;
 		}
-		return false;
+		Player p = (Player) sender;
+
+		// myPlugins.countdown only
+		if(!p.hasPermission("myPlugins.countdown")) {
+			p.sendMessage("§cDazu hast du keine Rechte!");
+			return false;
+		}
+
+		// dont place the countdown inside the player
+		Location cd_loc = p.getLocation().clone();
+		cd_loc.add(0, 0, 3);
+
+		CountdownDrawer drawer = new CountdownDrawer(cd_loc);
+		// 20 ticks are 1 second
+		BukkitTask task = Bukkit.getScheduler().runTaskTimer(Main.getPlugin(), drawer, 1, 20);
+		drawer.task = task;
+
+		return true;
 	}
-	
+
+	public static class CountdownDrawer implements Runnable {
+
+		private Location at;
+		private int seconds;
+		private BukkitTask task = null;
+
+		public CountdownDrawer(Location at) {
+			seconds = 100;
+			this.at = at;
+		}
+
+
+		@Override
+		public void run() {
+			if(task == null) return;
+
+			int mins = seconds/60;
+			int secs = seconds%60;
+
+			String mins_str = Integer.toString(mins);
+			String secs_str = Integer.toString(secs);
+
+			// add starting zero if necessary
+			if(mins_str.length() < 2) mins_str = "0" + mins_str;
+			if(secs_str.length() < 2) secs_str = "0" + secs_str;
+
+			TextDrawer.writeString(at, mins_str + ":" + secs_str);
+			Bukkit.broadcastMessage("[debug] countdown at " + seconds + "s");
+
+
+			seconds--;
+
+			if(seconds == 0) {
+				TextDrawer.writeString(at, "--:--");
+				Bukkit.broadcastMessage("countdown has finished!");
+				task.cancel();
+			}
+		}
+	}
 }
