a54a56f8e8be1e607b54e510ad6aad77b210ff60
Thejotanic <44734608+Thejotanic@users.noreply.github.com>
2018-11-23 00:29:39 +0100

    Add files via upload

--- MyPlugin/src/de/thejotanic/myPlugin/countdown/SchedulerChat.java
+++ MyPlugin/src/de/thejotanic/myPlugin/countdown/SchedulerChat.java
@@ -11,78 +11,51 @@ import de.thejotanic.myPlugin.main.Main;
 
 public class SchedulerChat implements CommandExecutor {
 	
-	private int taskID0;
-	private int taskID1;
-	private int taskID2;
+	private int taskID;
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
 		if(sender instanceof Player) {
 			Player p = (Player) sender;
-			if(p.hasPermission("myPlugins.countdown")) {
+			if(p.hasPermission("myPlugin.countdown")) {
 				if(args.length == 0) {
 					
-					taskID0 = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(), new Runnable() {
-									
-						int minutes = 6;
+					taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(), new Runnable() {
+						
+						int tenminutes = 0;
+						int minutes = 4;
+						int tenseconds = 5;
+						int seconds = 9;
 									
 						@Override
 						public void run() {
-										
-							minutes--;
-										
-							if(minutes < 5) {
-								
-								taskID1 = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(), new Runnable() {
-												
-									int tenseconds = 6;
-														
-									@Override
-									public void run() {
-													
-										tenseconds--;
-													
-										taskID2 = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(), new Runnable() {
-															
-											int seconds = 10;
-													
-											@Override
-											public void run() {
-															
-												seconds--;
-																
-												Bukkit.broadcastMessage("§e0" + minutes + ":" + tenseconds + seconds);
-												
-												if(seconds <= 0) {
-													Bukkit.getScheduler().cancelTask(taskID2);
-													if(tenseconds <= 0) {
-														Bukkit.getScheduler().cancelTask(taskID1);
-														if(minutes <= 0) {
-															
-															Bukkit.getScheduler().cancelTask(taskID0);
-														}
-																	
-													}
-																
-												}
-															
-											}
-														
-										}, 0, 20);
-										
+							
+							Bukkit.broadcastMessage("§e" + tenminutes + minutes + ":" + tenseconds + seconds);
+							
+							seconds--;
+							if(seconds < 0) {
+								tenseconds--;
+								if(tenseconds < 0) {
+									minutes--;
+									if(minutes < 0) {
+										tenminutes--;
+										if(tenminutes <= 0 && minutes <= 0 && tenseconds <= 0 && seconds <= 0) {
+											Bukkit.getScheduler().cancelTask(taskID);
+										}
+										minutes = 9;
 									}
-													
-								}, 0, 200);	
-											
-							} else 
-								Bukkit.broadcastMessage("§6" + minutes);		
+									tenseconds = 5;
+								}
+								seconds = 9;
+							}
+							
 						}
 						
-					}, 0, 1200);
+					}, 0, 20);
 				
 				} else
-					p.sendMessage("§cBitte benutze /countdown <x> <y> <z>");
+					p.sendMessage("§cBitte benutze /chatcountdown!");
 			} else
 				p.sendMessage("§cDazu hast du keine Rechte!");
 		}
