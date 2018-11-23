e9d82830d5283095ff3cddd7140a30e01613b657
Thejotanic <44734608+Thejotanic@users.noreply.github.com>
2018-11-23 19:55:56 +0100

    Add files via upload

--- MyPlugin/src/de/thejotanic/myPlugin/countdown/Scheduler.java
+++ MyPlugin/src/de/thejotanic/myPlugin/countdown/Scheduler.java
@@ -1,4 +1,5 @@
 package de.thejotanic.myPlugin.countdown;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -7,75 +8,87 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 
-import de.thejotanic.myPlugin.TextDrawer;
-import de.thejotanic.myPlugin.main.Main;
-
 public class Scheduler implements CommandExecutor {
+	
+	private Location loc;
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-		// player only
+		
 		if(!(sender instanceof Player)) {
 			return false;
 		}
+		
 		Player p = (Player) sender;
-
-		// myPlugins.countdown only
-		if(!p.hasPermission("myPlugins.countdown")) {
+		
+		if(!p.hasPermission("myPlugin.countdown")) {
 			p.sendMessage("§cDazu hast du keine Rechte!");
 			return false;
 		}
-
-		// dont place the countdown inside the player
-		Location cd_loc = p.getLocation().clone();
-		cd_loc.add(0, 0, 3);
-
-		CountdownDrawer drawer = new CountdownDrawer(cd_loc);
-		// 20 ticks are 1 second
-		BukkitTask task = Bukkit.getScheduler().runTaskTimer(Main.getPlugin(), drawer, 1, 20);
+		
+		if(args.length != 3) {
+			p.sendMessage("§cBitte benutze §6/scheduler <x> <y> <z>§c!");
+			return false;
+		}
+		
+		try {
+			
+			int x = Integer.valueOf(args[0]);
+			int y = Integer.valueOf(args[1]);
+			int z = Integer.valueOf(args[2]);
+			
+			this.loc = new Location(p.getWorld(), x, y, z);
+			
+		} catch (Exception e) {
+			p.sendMessage("§cFehler beim Übergeben der Werte!");
+			p.sendMessage("§cBitte benutze Zahlen!");
+		}
+		
+		CountdownDrawer drawer = new CountdownDrawer(loc);
+		BukkitTask task = Bukkit.getScheduler().runTaskTimer(Main.getPlugin(), drawer, 100, 20);
 		drawer.task = task;
-
+		
 		return true;
 	}
-
+	
 	public static class CountdownDrawer implements Runnable {
-
-		private Location at;
+		
+		private Location loc;
 		private int seconds;
 		private BukkitTask task = null;
-
-		public CountdownDrawer(Location at) {
-			seconds = 100;
-			this.at = at;
+		
+		public CountdownDrawer(Location loc) {
+			seconds = 300;
+			this.loc = loc;
 		}
-
 
 		@Override
 		public void run() {
+			
 			if(task == null) return;
-
-			int mins = seconds/60;
-			int secs = seconds%60;
-
-			String mins_str = Integer.toString(mins);
-			String secs_str = Integer.toString(secs);
-
-			// add starting zero if necessary
-			if(mins_str.length() < 2) mins_str = "0" + mins_str;
-			if(secs_str.length() < 2) secs_str = "0" + secs_str;
-
-			TextDrawer.writeString(at, mins_str + ":" + secs_str);
-			Bukkit.broadcastMessage("[debug] countdown at " + seconds + "s");
-
-
+			
+			int minutes = seconds/60;
+			int seconds = this.seconds%60;
+			
+			String min_str = Integer.toString(minutes);
+			String sec_str = Integer.toString(seconds);
+			
+			if(min_str.length() < 2) min_str = "0" + min_str;
+			if(sec_str.length() < 2) sec_str = "0" + sec_str;
+			
+			TextDrawer.writeString(loc, min_str + ":" + sec_str);
+			Bukkit.broadcastMessage("§8[debug] countdown at §e" + seconds + "§8sec!");
+			
 			seconds--;
-
-			if(seconds == 0) {
-				TextDrawer.writeString(at, "--:--");
-				Bukkit.broadcastMessage("countdown has finished!");
+			
+			if(seconds <= 0) {
+				TextDrawer.writeString(loc, "     ");
+				Bukkit.broadcastMessage("§aDer Countdown ist abgelaufen!");
 				task.cancel();
 			}
+			
 		}
+
 	}
+	
 }
